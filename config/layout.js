bb58229832d34caff5306b413b7644c62802cd2e
hongphipham97@gmail.com <hongphiham97@gmail.com>
2020-01-21 20:17:49 +0700

    add field to admin models, change layout items for each role

--- config/layout.js
+++ config/layout.js
@@ -5,4 +5,13 @@ module.exports = {
       update: 'Admin.update',
     },
   },
+  ADMIN: {
+    pluginsToHide: ['content-type-builder'],
+  },
+  MANAGER: {
+    pluginsToHide: ['content-type-builder', 'users-permissions'],
+  },
+  STAFF: {
+    pluginsToHide: ['content-type-builder', 'users-permissions'],
+  },
 };
